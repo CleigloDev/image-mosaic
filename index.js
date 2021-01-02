cddc4b29ecf19e2b64f2549a85c3e626a1d72dec
Carlo Lunetta <c.lunetta@reply.it>
2021-01-02 15:41:19 +0100

    add image color map

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-"use strict";
-
 import { getAverageColor } from 'fast-average-color-node';
+import FastAverageColor from 'fast-average-color';
 import mergeImages  from 'merge-images';
 import fs from "fs";
 import canvas from 'canvas';
 import sizeOf from 'image-size';
 import sharp from 'sharp';
-const { Canvas, Image } = canvas;
 
+
+const { Canvas, Image, loadImage, createCanvas } = canvas;
+const fac = new FastAverageColor();
+
+// ------------------------ Image Definition --------------------------------- //
+const image = await loadImage('./imagesIn/in.jpg');
+const naturalWidth = image.width;
+const naturalHeight = image.height;
+const imageCanvas = createCanvas(naturalWidth, naturalHeight);
+const context = imageCanvas.getContext('2d');
+context.drawImage(image, 0, 0);
+// ------------------------ Image Definition --------------------------------- //
+
+
+const { width, height } = sizeOf('./imagesIn/in.jpg');
+const mosaicSquareDimension = 25;
+
+var colorMap = {};
 var mosaicColors = {};
 const testImagesPath = ['./images/test.jpg', './images/test1.jpg'];
+
+const defineColorMapMosaicImage = () => {
+    for(var y = 0; y < height; y += 25) {
+        for(var x = 0; x < width; x += 25) {
+            let colorValue = getAreaColor(x, y, mosaicSquareDimension, mosaicSquareDimension).value;
+            colorMap[x+"-"+y] = {
+                "r": colorValue[0],
+                "g": colorValue[1],
+                "b": colorValue[2]
+            }
+        }
+    }
+};
+
+const defineFinalColorMap = () => {
+    const keys = Object.keys(colorMap);
+};
+
+const getAreaColor = (xCoords, yCoords, frameWidth, frameHeight) => {
+    const imageData = context.getImageData(xCoords, yCoords, frameWidth, frameHeight);
+    return fac.prepareResult(fac.getColorFromArray4(imageData.data, {}));
+};
 
 const getMosaicImageColor = (aImagesPath) => {
     return new Promise((resolve, reject) => {
@@ -21,8 +59,8 @@ const getMosaicImageColor = (aImagesPath) => {
                     let imageColors = {};
                     imageColors[imagePath] = {
                         "r": color.value[0],
-                        "g": color.value[0],
-                        "b": color.value[0]
+                        "g": color.value[1],
+                        "b": color.value[2]
                     };
                     resolve(imageColors);
                 })
@@ -44,12 +82,13 @@ const getMosaicImageColor = (aImagesPath) => {
             reject(err);
         })
     });
-}
+};
 
-const resizeImage = (imagePath) => {
+const resizeImage = (imagePath, options) => {
     return new Promise((resolve, reject) => {
+        options = options ? options : { width: mosaicSquareDimension, height: mosaicSquareDimension };
         sharp(imagePath)
-        .resize({ width: 50, height: 50 })
+        .resize(options)
         .toBuffer()
         .then(data => {
             const base64String = data.toString("base64");
@@ -95,30 +134,34 @@ const generateMosaic = (aImagesPath) => {
             reject(err);
         });
     });
-}
+};
 
-const testResized = resizeImage('./images/test.jpg');
-const test1Resized = resizeImage('./images/test1.jpg');
+defineColorMapMosaicImage();
 
-getMosaicImageColor(testImagesPath)
-.then(() => {
-    Promise.all([
-        testResized,
-        test1Resized
-    ])
-    .then(() => {
-        console.log("Resize success!");
-        generateMosaic(testImagesPath)
-        .then(() => {
-            console.log("Images merged!");
-        })
-        .catch((err) => {
-            console.error(err);
-        })
-    })
-    .catch((err) => {
-        console.error(err);
-    });    
-});
+// resizeImage('./imagesIn/in.jpg', {width: 5000, height: 2500});
+
+// const testResized = resizeImage('./images/test.jpg');
+// const test1Resized = resizeImage('./images/test1.jpg');
+
+// getMosaicImageColor(testImagesPath)
+// .then(() => {
+//     Promise.all([
+//         testResized,
+//         test1Resized
+//     ])
+//     .then(() => {
+        // console.log("Resize success!");
+        // generateMosaic(testImagesPath)
+        // .then(() => {
+        //     console.log("Images merged!");
+        // })
+        // .catch((err) => {
+        //     console.error(err);
+        // });
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//     });    
+// });
 
 
